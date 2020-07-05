a25d6fb045cd29e19ecc3b1f08ae183dd7a27103
Chris Adie <chris.adie@googlemail.com>
2020-07-05 09:17:47 +0100

    Initial framework

---
+++ README.md
@@ -0,0 +1,10 @@
+# node-red-contrib-ais-decoder
+A Node-Red node to decode AIS message strings
+
+## Install
+Run the following npm command in your Node-RED user directory (typically ~/.node-red):
+```
+npm install node-red-contrib-ais-decoder
+```
+
+## Usage
---
+++ ais-decoder/ais-decoder.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function AisDecoder(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("ais-decoder",AisDecoder);
+}
