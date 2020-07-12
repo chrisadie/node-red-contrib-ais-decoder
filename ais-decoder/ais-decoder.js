89691abf5d44d4b9d28b57b28df5742cea450f82
Chris Adie <chris.adie@googlemail.com>
2020-07-12 14:24:16 +0100

    Second attempt; outer decode only

--- ais-decoder/ais-decoder.js
+++ ais-decoder/ais-decoder.js
@@ -4,6 +4,7 @@
 //
 // License: see LICENSE file
 
+
 //
 // Register a creator function for ais-decoder
 //
@@ -12,167 +13,186 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         var nodeContext = node.context();
-        nodeContext.set("tDataPacketQueue",[]);
+        nodeContext.set("fragmentList",[]);
         node.on('input', function(msg) {
-        		var m = processDatagram(node,msg);
-        		if (m!=null) {
-        			node.send(m);
-        		}
+        		processDatagram(node,msg);
         });
     }
     RED.nodes.registerType("ais-decoder",AisDecoder);
 }
 
 //
-// Divide datagram into AIVDM data packets, queue them and process the queue
+// Divide datagram into AIVDM data fragments, process them, and
+// send resulting message[s] to the next node.
 //
 function processDatagram (node,msg) {
+	var m = [];
 	var f = msg.payload.split('\r');
 	for (i=0;i<f.length;i++) {
-		queuePacket(node,f[i]);
-	}
-	return processPacketQueue(node,msg);
-}
-
-//
-// Add a data packet to the end of the queue
-//
-function queuePacket(node,dataPacket) {	
-	var p = {};
-	p.timeStamp = new Date();
-	p.dataPacket = dataPacket;
-   var q = node.context().get("tDataPacketQueue");
-   q.push(p);
-   node.context().set("tDataPacketQueue",q);
-}
-
-//
-// Process the packet queue, returning msg, or null if there
-// is not enough unexpired data in the queue.
-//
-function processPacketQueue(node,msg) {
-	var aisPayload = null;
-   var q = node.context().get("tDataPacketQueue");
-   //timeoutPackets(q);
-   console.log("length of Q = "+q.length);
-	// Remove old/invalid data packets from the head of the queue
-	while (q.length>0 && isInvalid(q[0])) {
-		console.log("removed invalid packet");
-		q.shift();
-	}
-	if (q.length>0) {
-		// Construct a data payload from one or more data packets
-		aisPayload = parsePackets(q);
-	}
-   node.context().set("tDataPacketQueue",q);
-	if (aisPayload==null || msg==null) {
-		return null;
-	} else {
-		msg.aisPayload = aisPayload;
-		return msg;
-	}
-}
-
-//
-// If the timestamp on the timed data packet is older than
-// the configured lifetime, invalidate the packet.
-//
-// ToDo: Make lifetime configurable.
-//
-function timeoutPackets(q) {
-	var d = new Date();
-	var lifetime = 15000; // Milliseconds
-	for (i=0;i<q.length;i++) {
-		if (q[i].timeStamp.getTime()+lifetime>d.getTime()) {
-			invalidatePacket(q[i]);
-		}
-	}
-}
-
-//
-// Return false if the packet is empty, otherwise true.
-//
-function isInvalid(p) {
-	if (p.dataPacket.length == 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-//
-// Using the data packet at the head of the queue, collect any other
-// related data packets and concatenate the encoded payloads. Return
-// the information parsed out of that payload, or null if error.
-//
-function parsePackets(q) {
-	var aisPayload = {};
-	var f1 = parseFragment(q[0].dataPacket);
-	aisPayload.dataPayload = f1.fData;
-	if (f1.fNumber!=1) {
-		// Not the first fragment, so assume the first fragment is missing.
-		invalidatePacket(q[0]);
-		return null;
-	}
-	if (f1.fCount==1) {
-		// Single-fragment message
-		invalidatePacket(q[0]);
-		return aisPayload;
-	}
-	// Multi-fragment message. Find the other fragments, assumed to be in order.
-	var consumed = [];
-	var completed = false;
-	var seekingFragmentNumber = 2;
-	for (i=1;i<q.length;i++) {
-		var fN = parseFragment(q[i].dataPacket);
-		if(fN.fCount==f1.fCount
-			&& fN.fMessageId==f1.fMessageId
-			&& fN.fRadioChannel==f1.fRadioChannel
-			&& fN.fNumber==seekingFragmentNumber) {
-		 	// This fragment is for us
-			aisPayload.dataPayload += fN.fData;
-			consumed.push(q[i]);
-			if (fN.fNumber==fN.fCount) {
-				// It is the last fragment
-				completed = true;
-				break;
+		var frag = parseFragment(f[i]);
+		if (frag!=null) {
+			// Put the message into an array, in case we have to send more than one message
+			if (m.length==0) {
+				m[0] = msg;
+			} else {
+				m.push({});
 			}
-			// Go round and look for next fragment
-			seekingFragmentNumber++;
+			m[m.length-1].payload = f[i];
+			if (frag.fCount==1) {
+				// Single-fragment AIVDM sentence
+				m[m.length-1].aisData = decodeAisData(frag.fData);
+			} else {
+				// Multi-fragment AIVDM sentence
+				var mf = processMultiFragments(node,frag);
+				if (mf!=null) {
+					// We have a complete AIVDM sentence to return
+					m[m.length-1].aisData = decodeMultiFrag(mf);
+				} else {
+					// AIVDM sentence still incomplete - we can't return it
+					m.pop();
+				}
+			}
 		}
 	}
-	if (completed) {
-		// All fragments found
-		invalidatePacket(q[0]);
-		consumed.forEach(invalidatePacket);
-		return aisPayload;
-	} else {
-		// Not all fragments available yet
-		// ToDo: prevent message with missing fragments from blocking other messages
-		return null;
+	if (m.length>0) {
+		var mm = [m];
+		node.send(mm);
 	}
 }
 
 //
-// Invalidate a queued packet by zeroing the timestamp.
+// Add a fragment to the list of fragments, returning a list of fragments
+// if all fragments have been recieved.
 //
-function invalidatePacket(p) {
-	p.dataPacket = "";
+function processMultiFragments(node,frag) {	
+	var list = node.context().get("fragmentList");
+	addFragmentToList(list,frag);
+	var f = extractComplete(list);
+	removeUnwanted(list);
+	return f;
 }
 
 //
-// Parse an AIVDM fragment and return the decoded information
+// Add a fragment to the list of arrays of fragments
+//
+function addFragmentToList(list,frag) {
+	var item;
+	for (i=0;i<list.length;i++) {
+		item = list[i];
+		if (item.length>0
+			&& !item[0].unwanted
+			&& !item[0].complete
+			&& item[0].fCount==frag.fCount
+			&& item[0].fMessageId==frag.fMessageId
+			&& item[0].fRadioChannel==frag.fRadioChannel) {
+			if (item.length+1==frag.fNumber) {
+				// We have the next fragment!
+				item.push(frag);
+				if (frag.fNumber==frag.fCount) {
+					// It is the last fragment!
+					item[0].complete = true;
+				}
+			} else {
+				// Out-of-sequence fragment
+				// Mark sequence unwanted
+				item[0].unwanted = true;
+			}
+			break;
+		}
+  	}
+	if (frag.fNumber==1) {
+		// First fragment of a new sentence
+		frag.timeStamp = new Date();
+		item = [frag];
+		list.push(item);
+   }
+}
+
+//
+// Extract and return a complete fragment sequences from the list
+//
+function extractComplete(list) {
+	var seq;
+	var i = 0;
+	while (i<list.length) {
+		seq = list[i];
+		if (seq[0].complete) {
+			// Remove the sequence
+			list.splice(i,1);
+			return seq;
+		} else {
+			// Next sequence
+			i++;
+		}
+	}
+	return null;
+}
+
+//
+// Remove unwanted fragment sequences from the list
+//
+function removeUnwanted(list) {
+	var seq;
+	var i = 0;
+	var d = new Date();
+	while (i<list.length) {
+		seq = list[i];
+		if (seq[0].timeStamp.getTime()+15000<d.getTime()) {
+			seq[0].unwanted = true;
+		}
+		if (seq[0].unwanted) {
+			// Remove the sequence
+			list.splice(i,1);
+		} else {
+			// Next sequence
+			i++;
+		}
+	}
+}
+
+//
+// Parse and validate an AIVDM fragment and return the decoded information,
+// or null if there's a parse error.
 //
 function parseFragment(frag) {
 	var result = {};
 	var f = frag.split(',');
+	if (f.length!=7) {
+		return null;
+	}
+	if (f[0]!="!AIVDM") {
+		return null;
+	}
+	result.fHead = f[0];
 	result.fCount = parseInt(f[1], 10);
 	result.fNumber = parseInt(f[2], 10);
+	if (result.fCount==NaN || result.fNumber==NaN) {
+		return null;
+	}
 	result.fMessageId = parseInt(f[3], 10);
 	result.fRadioChannel = f[4];
 	result.fData = f[5];
 	result.fChecksum = f[6];
-	console.log("parseFragment: "+f[5]);
+	// ToDo: validate checksum
 	return result;
+}
+
+//
+// Take an array of sentence fragments, concatenate the data, and return
+// the decoded information.
+//
+function decodeMultiFrag(fArray) {
+	var data = ""
+	for (i=0;i<fArray.length;i++) {
+		data += fArray[i].fData;
+	}
+	return decodeAisData(data);
+}
+
+//
+// Decode an AIS data string
+//
+function decodeAisData(data) {
+	// @@@
+	return data;
 }
